48a5925ff0de98de04a5f6ae06476c092a5532d2
amoljagtap17 <amoljagtap17@gmail.com>
2024-07-30 18:22:31 +0530

    micro-services-frontend: update vite config

--- app-container/vite.config.ts
+++ app-container/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: 'localhost',
+    port: 4000,
+    strictPort: true,
+    open: true,
+  },
+  preview: {
+    host: 'localhost',
+    port: 4000,
+    strictPort: true,
+    open: true,
+  },
+});
