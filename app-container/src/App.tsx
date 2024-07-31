ab22d2a65dcf76438922a17a99f196721e0cd059
amoljagtap17 <amoljagtap17@gmail.com>
2024-07-31 23:37:54 +0530

    micro-services-frontend: add createGlobalState to maintain global state

--- app-container/src/App.tsx
+++ app-container/src/App.tsx
@@ -1,11 +1,23 @@
 import { Container, Grid } from '@mui/material';
+import { createGlobalState } from './app/utils';
+
+type IContainerGlobalState = {
+  name: string;
+};
+
+const useContainerState = createGlobalState<IContainerGlobalState>(
+  'app-container',
+  { name: 'John Doe' },
+);
 
 export function App() {
+  const { data } = useContainerState();
+
   return (
     <Container maxWidth="lg">
       <Grid container spacing={2}>
         <Grid item xs={12} lg={4}>
-          Home
+          Home | {data?.name}
         </Grid>
       </Grid>
     </Container>
